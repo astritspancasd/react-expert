c9f70fc2f90126284ddeb97e35635ad903ab2be0
astrit spanca <aspanca@ikub.local>
2022-03-21 23:17:39 +0100

    Utils

---
+++ src/utils/storage.ts
@@ -0,0 +1,16 @@
+import * as ls from "local-storage";
+
+export const storage = {
+  add: (key: string, value: string): void => {
+    ls.set(key, value);
+  },
+  remove: (key: string): void => {
+    ls.remove(key);
+  },
+  get: (key: string): string => {
+    return ls.get(key);
+  },
+  clear: (): void => {
+    ls.clear();
+  },
+};
