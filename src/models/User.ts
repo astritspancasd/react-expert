5d80f5c2ad8502de00bd6efd6f6281533786eeac
astrit spanca <aspanca@ikub.local>
2022-03-21 21:29:23 +0100

    Models. __mocks__, updated readme

---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+import { Address } from "./Address";
+import { Company } from "./Company";
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Address;
+  company: Company;
+}
